d4121f712804567248d907c670e0989cb50d7204
binoy638 <binoy638@gmail.com>
2022-08-28 10:43:11 +0530

    test

--- test.py
+++ test.py
@@ -16,30 +16,42 @@ playlist = [getFullPath('dog.mp4'),getFullPath('dog2.mp4')]
 
 # def generatePlaylist(inpath):
 #     return [f for f in listdir(inpath) if isfile(join(inpath, f))]
-
-def main(args, loglevel):
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
-
-    #[omx_command.append(f) for f in args.remaining] # add extra OMX commands to end, sort of janky
-    # playlist = generatePlaylist(args.directory)
-
+# def main():
+    
+def main():
+    # for f in playlist:
+    #     full_command = omx_command +  [f]
+    #     stdout = subprocess.PIPE
+    #     proc = None
+    #     try:
+    #         # logging.debug("playing: {0}".format(full_path))
+    #         print(f'playing: {f}')
+    #         proc = subprocess.run(full_command, check=True, stdin=subprocess.PIPE, stdout=stdout, close_fds=True)
+    #     except KeyboardInterrupt:
+    #         if proc is not None:
+    #             proc.kill()
+    #         # logging.info("Keyboard Interrupt")
+    #         sys.exit()
+    #     except Exception as e:
+    #         # logging.exception()
     for f in playlist:
-        full_path = args.directory + "/" + f
-        full_command = omx_command + args.remaining + [full_path]
-
-        stdout = subprocess.PIPE
-        if args.debug:
-            stdout = False
-
-        proc = None
+        full_command = omx_command + [f]
+        sys.stdout = subprocess.PIPE 
+        proc = None 
         try:
-            logging.debug("playing: {0}".format(full_path))
-            proc = subprocess.run(full_command, check=True, stdin=subprocess.PIPE, stdout=stdout, close_fds=True)
+            print(f'playing: {f}')
+            proc = subprocess.run(full_command,check=True,stdin=subprocess.PIPE,stdout=sys.stdout,close_fds=True)
         except KeyboardInterrupt:
             if proc is not None:
-                proc.kill()
-            logging.info("Keyboard Interrupt")
+                proc.kill() 
             sys.exit()
         except Exception as e:
-            logging.exception()
+            print(e)
+
+    print('done')
+
+
+
+main()
+
 
